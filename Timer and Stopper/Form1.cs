323a2633980dc796bae9baeab30540ca9e772ed9
Stefan677890 <szymonmisztal2626@gmail.com>
2026-03-18 21:53:10 +0100

    Version 0.3
    Made it work

--- Timer and Stopper/Form1.cs
+++ Timer and Stopper/Form1.cs
@@ -1,57 +1,61 @@
+using System;
+using System.Windows.Forms;
+
 namespace Timer_and_Stopper
 {
     public partial class Form1 : Form
     {
-        int allseconds = 0;
-        int all_seconds_stoper = 0;
+
+        int allseconds = 0;        
+        int all_seconds_stoper = 0; 
+
         public Form1()
         {
             InitializeComponent();
             timer1.Interval = 1000;
             timer2.Interval = 1000;
+
+            timer1.Tick += timer1_Tick;
         }
 
-        private void tabPage1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Tabs_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Time1_Click(object sender, EventArgs e)
-        {
-
-        }
 
         private void start_Click(object sender, EventArgs e)
         {
-            if (int.TryParse(minutes_txt.Text, out int minutes ))
+ 
+            if (int.TryParse(minutes_txt.Text, out int minutes) && int.TryParse(seconds_txt.Text, out int seconds))
             {
-                if (int.TryParse(seconds_txt.Text, out int seconds))
-                {
-                    allseconds = minutes * 60 + seconds;
-                    Time.Text = TimeSpan.FromSeconds(allseconds).ToString(@"mm\:ss");
-                }
-                else
-                {
-                    MessageBox.Show("Poda³eœ nie prawid³owy format czasu");
-                    return;
-                }
+                allseconds = minutes * 60 + seconds;
+                Time.Text = TimeSpan.FromSeconds(allseconds).ToString(@"mm\:ss");
+
+                timer1.Start();
+            }
+            else
+            {
+                MessageBox.Show("Poda³eœ nieprawid³owy format czasu.");
             }
         }
 
         private void stop_Click(object sender, EventArgs e)
         {
 
+            timer1.Stop();
         }
+
+  
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (allseconds > 0)
+            {
+                allseconds--;
+                Time.Text = TimeSpan.FromSeconds(allseconds).ToString(@"mm\:ss");
+            }
+            else
+            {
+                timer1.Stop();
+                MessageBox.Show("Time ended");
+            }
+        }
+
 
         private void start1_Click(object sender, EventArgs e)
         {
@@ -65,18 +69,22 @@ namespace Timer_and_Stopper
 
         private void reset_Click(object sender, EventArgs e)
         {
-            timer2.Dispose();
+
+            timer2.Stop();
+            all_seconds_stoper = 0;
+            Time1.Text = "00:00";
         }
 
         private void timer2_Tick(object sender, EventArgs e)
         {
-            int allseconds = 0;
-            
+            all_seconds_stoper++;
+            Time1.Text = TimeSpan.FromSeconds(all_seconds_stoper).ToString(@"mm\:ss");
         }
 
-        private void Time_Click(object sender, EventArgs e)
-        {
-            
-        }
+        private void tabPage1_Click(object sender, EventArgs e) { }
+        private void label1_Click(object sender, EventArgs e) { }
+        private void Tabs_Click(object sender, EventArgs e) { }
+        private void Time1_Click(object sender, EventArgs e) { }
+        private void Time_Click(object sender, EventArgs e) { }
     }
 }
